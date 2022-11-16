2b705ee000e8a86ce28e658e48f3e1db75a05a42
srivanichellamuthu <114242737+srivanichellamuthu@users.noreply.github.com>
2022-11-17 00:24:34 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/db2.py
@@ -0,0 +1,29 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import ibm_db
+... conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=ea286ace-86c7-4d5b-8580-3fbfa46b1c66.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31505;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=plq76207;PWD=ZmeqbSx0vyVOuxoG;","","")
+... print("connection successfully")
+... 
+... 
+... sql="""CREATE TABLE login (
+...     Name varchar(255),
+...     Email varchar(255),
+...     Password varchar(255) )"""
+... print("Added successfully")
+... ibm_db.exec_immediate(conn,sql)
+... 
+... sql="""CREATE TABLE Customerdetails (
+...     Name varchar(255),
+...     ShopName varchar(255),
+...     Location varchar(255),
+...     MobileNumber varchar(255) )"""
+... print("Added successfully")
+... ibm_db.exec_immediate(conn,sql)
+... 
+... 
+... """{% with messages = get_flashed_messages(with_categories=true) %}
+...       {% if messages %}
+...         {% for category, messages in messages %}
+...           <div class="alert alert-{{category}}" ></div>{{messages}}</div>
+...         {% endfor %}
+...   {% endif %}  
